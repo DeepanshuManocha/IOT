09b38e52e3e2d359a2d125739b34b2977f1e30ef
DeepanshuManocha <dmanocha1509@gmail.com>
2020-05-17 17:15:10 +0530

    IOT Project

---
+++ Assets/ReceiveResult.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReceiveResult : MonoBehaviour {
+
+    public GameObject Lighton;
+    public GameObject Lightoff;
+    public AudioSource okaysound;
+	// Use this for initialization
+	void Start () {
+        //GameObject.Find("Text").GetComponent<Text>().text = "You need to be connected to Internet";
+	}
+	
+    void onActivityResult(string recognizedText){
+        char[] delimiterChars = {'~'};
+        string[] result = recognizedText.Split(delimiterChars);
+
+        //You can get the number of results with result.Length
+        //And access a particular result with result[i] where i is an int
+        //I have just assigned the best result to UI text
+        GameObject.Find("Text").GetComponent<Text>().text = result[0];
+        //string[] a="Alexa turn on the light"; 
+        if(result[0]=="Alexa turn on the light")
+        {
+            okaysound.Play();
+            Lighton.SetActive(true);
+            Lightoff.SetActive(false);
+        }
+        else if(result[0]=="Alexa turn off the light")
+        {
+            okaysound.Play();
+            Lighton.SetActive(false);
+            Lightoff.SetActive(true);
+        }
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void SmartCity()
+    {
+        SceneManager.LoadScene("SmartCity");
+    }
+    public void SmartHome()
+    {
+        SceneManager.LoadScene("SmartHome");
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("UI");
+    }
+    public void doExitGame() 
+    {
+        Application.Quit();
+    }
+}
